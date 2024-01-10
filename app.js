4faf786da1240c9d7d3b22cd12fcf5b1591acfd7
Matti Luukkainen <mluukkai@iki.fi>
2024-01-10 21:32:51 +0200

    deploy

--- app.js
+++ app.js
@@ -7,7 +7,12 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
+app.get('/test', (request, response) => {
+  res.send('<h1>Hello World!</h1>')
+})
+
 app.listen(PORT, () => {
+  console.log('ENV', process.env.PORT)
   // eslint-disable-next-line no-console
-  console.log('server started on port 5000')
+  console.log(`server started on port ${PORT}`)
 })
