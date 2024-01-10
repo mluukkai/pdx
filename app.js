60af71e719f1289785ccaa521938a91f22ebde82
Matti Luukkainen <mluukkai@iki.fi>
2024-01-10 21:35:02 +0200

    fix lint

--- app.js
+++ app.js
@@ -7,12 +7,7 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
-app.get('/test', (request, response) => {
-  res.send('<h1>Hello World!</h1>')
-})
-
 app.listen(PORT, () => {
-  console.log('ENV', process.env.PORT)
   // eslint-disable-next-line no-console
   console.log(`server started on port ${PORT}`)
 })
