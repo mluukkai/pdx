f99c11909788cca286d4141d5521da6325f865ba
Matti Luukkainen <mluukkai@iki.fi>
2024-01-11 16:13:42 +0200

    error...

--- app.js
+++ app.js
@@ -12,10 +12,11 @@ app.get('/version', (req, res) => {
 })
 
 app.get('/health', (req, res) => {
+  throw 'error...'
   res.send('ok')
 })
 
-app.listen(PORT + 1, () => {
+app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`server started on port ${PORT}`)
 })
