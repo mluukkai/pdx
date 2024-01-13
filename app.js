4eb93632fd1c0805fe1447a46faac2b978445da3
Matti Luukkainen <mluukkai@iki.fi>
2024-01-13 22:24:28 +0200

    do not deploy on pr

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/version', (req, res) => {
-  res.send('32')
+  res.send('33')
 })
 
 app.get('/health', (req, res) => {
