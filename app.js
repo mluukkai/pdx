de6b8357c09cbc814b9d70524177d606e8255e82
Matti Luukkainen <mluukkai@iki.fi>
2024-01-13 23:12:31 +0200

    refactory workflow

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/version', (req, res) => {
-  res.send('34')
+  res.send('35')
 })
 
 app.get('/health', (req, res) => {
