3ae327c66ad4c0907a3d3577e04716e260a79c56
Matti Luukkainen <mluukkai@iki.fi>
2024-01-13 13:43:31 +0200

    fix

--- app.js
+++ app.js
@@ -7,12 +7,12 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/version', (req, res) => {
-  res.send('30')
+  res.send('31')
 })
 
 app.get('/health', (req, res) => {
   // eslint-disable-next-line no-constant-condition
-  if (true) throw('problems')
+  if (false) throw('problems')
   res.send('ok')
 })
 
