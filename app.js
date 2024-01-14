902865f3b1de03bf2dcd438026734b544b5b4fd9
Matti Luukkainen <mluukkai@iki.fi>
2024-01-14 21:40:36 +0200

    version to 43

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.get('/version', (req, res) => {
 
 app.get('/health', (req, res) => {
   // eslint-disable-next-line no-constant-condition
-  if (true) throw('problems')
+  if (false) throw('problems')
   res.send('ok')
 })
 
