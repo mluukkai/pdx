fcac3ba0143ff5a4bb16967ec3b249e556a4a33e
Matti Luukkainen <mluukkai@iki.fi>
2024-01-14 12:18:36 +0200

    #skip deploy

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/version', (req, res) => {
-  res.send('37')
+  res.send('38')
 })
 
 app.get('/health', (req, res) => {
