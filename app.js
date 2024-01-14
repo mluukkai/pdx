cc4076ba3cec253e81fe1f5320bad3d704f32640
Matti Luukkainen <mluukkai@iki.fi>
2024-01-14 20:29:00 +0200

    fixing code

--- app.js
+++ app.js
@@ -18,5 +18,5 @@ app.get('/health', (req, res) => {
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
-  console.log(`server started on port ${PORT}`);
+  console.log(`server started on port ${PORT}`)
 })
