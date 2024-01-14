7fa68d77426b09d813df97c91288a26fb3b4a902
Matti Luukkainen <mluukkai@iki.fi>
2024-01-14 21:30:09 +0200

    version to 43, brkn

--- app.js
+++ app.js
@@ -7,12 +7,12 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/version', (req, res) => {
-  res.send('42')
+  res.send('43')
 })
 
 app.get('/health', (req, res) => {
   // eslint-disable-next-line no-constant-condition
-  if (false) throw('problems')
+  if (true) throw('problems')
   res.send('ok')
 })
 
