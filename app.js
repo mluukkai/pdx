e501c95c73adb11143c4049e1761da7e67eb2c2c
Matti Luukkainen <mluukkai@iki.fi>
2024-01-16 10:22:26 +0200

    moar deploy

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/version', (req, res) => {
-  res.send('44')
+  res.send('45')
 })
 
 app.get('/health', (req, res) => {
